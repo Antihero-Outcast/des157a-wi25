919bf4ae3d2b8574e357fce7f2799308392ddb9e
Salimah Rogers <salim@Mac.wavecable.com>
2025-02-21 00:13:06 -0800

    literally everything but event handling

--- madlibs/v2-final/script.js
+++ madlibs/v2-final/script.js
@@ -2,5 +2,18 @@
 {
     'use strict';
 
-    
+    const wand = document.querySelector('#wand');
+    const title = document.querySelector('#spellbook-title');
+
+    const page1 = document.querySelector('#pg1');
+    const page2 = document.querySelector('#pg2');
+    const page3 = document.querySelector('#pg3');
+
+    const pg1Next = document.querySelector('#page-nav1 .next');
+    const pg2Prev = document.querySelector('#page-nav2 .prev');
+    const pg2Next = document.querySelector('#page-nav2 .next');
+    const pg3Prev = document.querySelector('#page-nav3 .prev');
+    const pg3Finish = document.querySelector('#page-nav3 .finish');
+
+    const spellbook = document.querySelector('#overlay');
 })();
